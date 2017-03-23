e695d75127ec82736e96e58cea2d7e54c8f632e1
leepengcheng <zoomlion@qq.com>
2017-03-23 22:28:06 +0800

    convex_hull

---
+++ convex_hull.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data=np.random.randint(0,100,10).reshape([-1,2])
+# data=np.array([[0,0],[0,10],[15,10],[10,1]])
+data=data[data[:,1].argsort()]
+p0=data[0]
+print(p0)
+data1=data[1:,]
+vecs=[(p-p0).dot(np.array([1,0]))/np.linalg.norm(p-p0) for p in data1]#矢量集合
+orders=np.argsort(-np.array(vecs)) #矢量排序序号
+data1=data1[orders]
+data=np.vstack((p0,data1,p0))
+plt.scatter(data[:-1,0],data[:-1,1]) #散点图
+plt.plot(data[:,0],data[:,1]) 
+plt.show()
